3655bf080b28301dd918b17c1eed1820dd13e56e
Ireneusz Miernik <irekmiernik@gmail.com>
2024-05-22 22:00:21 +0200

    Moving HTML  from tasks-list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="h1">Lista zadań</h1>
+
+      <section className="section">
+        <div className="sectionFlex sectionFlex--header">
+          <h2 className="sectionFlex__itemContent h2">Dodaj nowe zadanie</h2>
+        </div>
+
+        <form className="sectionFlex">
+          <input className="sectionFlex__itemContent" placeholder="Co jest do zrobienia?" autofocus />
+          <button className="sectionFlex__itemButton">Dodaj zadanie</button>
+        </form>
+      </section>
+
+      <section className="section">
+
+        <div className="sectionFlex sectionFlex--header">
+          <h2 className="sectionFlex__itemContent h2">Lista zadań</h2>
+        </div>
+
+        <ul className="sectionTasksList"></ul>
+      </section>
+
+    </>
   );
 }
 
