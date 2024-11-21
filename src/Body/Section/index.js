a3acb9400811974527506e5ace6af849874f7bcc
irekmiernik <129183189+irekmiernik@users.noreply.github.com>
2024-11-21 17:24:54 +0100

    Removed className h2 for <h2>

--- src/Body/Section/index.js
+++ src/Body/Section/index.js
@@ -3,7 +3,7 @@ import './style.css';
 const Section = ({ title, body, twoButtons }) => (
     <section className="section">
         <div className="sectionFlex sectionFlex--header">
-            <h2 className="sectionFlex__itemContent h2">{title} </h2>
+            <h2 className="sectionFlex__itemContent">{title} </h2>
             {twoButtons}
         </div>
         {body}
